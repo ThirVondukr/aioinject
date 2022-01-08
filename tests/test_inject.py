444f58fcd1b45801f812ba8f337f032de467f52e
Doctor <thirvondukr@gmail.com>
2022-01-08 11:47:55 +0300

    test dependency injection with multiple dependencies of same type/interface

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -2,24 +2,60 @@ from typing import Annotated
 
 import pytest
 
-from dependency_depression import Callable, Depression, Inject, NoCache, inject, providers
+from dependency_depression import NoCache, inject, Depression, providers, Inject
 
 
-class _Test:
+class _Session:
     pass
 
 
+class _Service:
+    def __init__(self, session: Annotated[_Session, Inject]):
+        self.session = session
+
+
+class _Interface:
+    def __init__(self, session: Annotated[_Session, Inject]):
+        self.session = session
+
+
+class _ImplementationA(_Interface):
+    pass
+
+
+class _ImplementationB(_Interface):
+    pass
+
+
+def _get_impl_b(session: Annotated[_Session, Inject]) -> _ImplementationB:
+    return _ImplementationB(session)
+
+
+class _NeedsMultipleImplementations:
+    def __init__(
+        self,
+        a: Annotated[_Interface, Inject[_ImplementationA]],
+        b: Annotated[_Interface, Inject[_get_impl_b]],
+    ):
+        self.a = a
+        self.b = b
+
+
 @pytest.fixture
-def container():
-    depression = Depression()
-    depression.register(Callable(_Test))
-    return depression
+def container() -> Depression:
+    container = Depression()
+    container.register(providers.Callable(_Session))
+    container.register(providers.Callable(_Service))
+    container.register(providers.Callable(_ImplementationA, type_=_Interface))
+    container.register(providers.Callable(_get_impl_b, type_=_Interface))
+    container.register(providers.Callable(_NeedsMultipleImplementations))
+    return container
 
 
 @inject
 def _injectee(
-    test: Annotated[_Test, Inject],
-    test_no_cache: Annotated[_Test, Inject, NoCache],
+    test: Annotated[_Session, Inject],
+    test_no_cache: Annotated[_Session, Inject, NoCache],
 ):
     return test, test_no_cache
 
@@ -31,7 +67,7 @@ def test_would_fail_without_active_context():
 
 def test_would_not_inject_without_inject_marker(container):
     @inject
-    def injectee(test: _Test):
+    def injectee(test: _Session):
         pass
 
     with container.sync_context(), pytest.raises(TypeError):
@@ -40,8 +76,8 @@ def test_would_not_inject_without_inject_marker(container):
 
 def test_simple_inject(container):
     with container.sync_context():
-        test, *_ = _injectee()
-        assert isinstance(test, _Test)
+        session, *_ = _injectee()
+        assert isinstance(session, _Session)
 
 
 def test_no_cache_marker(container):
@@ -55,34 +91,38 @@ def test_no_cache_marker(container):
     assert no_cache_first is not no_cache_second
 
 
-def test_inject_concrete_implementation():
-    class Session:
-        pass
-
-    def get_session() -> Session:
-        return Session()
-
-    class Service:
-        def __init__(
-            self,
-            # If you don't care how session is created you can leave `Inject` empty
-            session: Annotated[Session, Inject],
-        ):
-            pass
-
-    @inject
-    def injectee(
-        service: Annotated[Service, Inject],
-        # Alternatively you can pass an implementation into Inject
-        session: Annotated[Session, Inject[get_session]],
-    ):
-        return service, session
-
-    container = Depression()
-    container.register(providers.Callable(get_session))
-    container.register(providers.Callable(Service))
-
+@pytest.mark.anyio
+async def test_simple_service(container):
     with container.sync_context() as ctx:
-        service, session = injectee()
-        assert isinstance(service, Service)
-        assert isinstance(session, Session)
+        session = ctx.resolve(_Session)
+        assert isinstance(session, _Session)
+
+    async with container.context() as ctx:
+        session = await ctx.resolve(_Session)
+        assert isinstance(session, _Session)
+
+
+@pytest.mark.anyio
+async def test_retrieve_service_with_dependencies(container):
+    with container.sync_context() as ctx:
+        service = ctx.resolve(_Service)
+        assert isinstance(service, _Service)
+        assert isinstance(service.session, _Session)
+
+    async with container.context() as ctx:
+        service = await ctx.resolve(_Service)
+        assert isinstance(service, _Service)
+        assert isinstance(service.session, _Session)
+
+
+@pytest.mark.anyio
+async def test_service_with_multiple_dependencies_with_same_type(container):
+    with container.sync_context() as ctx:
+        service = ctx.resolve(_NeedsMultipleImplementations)
+        assert isinstance(service.a, _ImplementationA)
+        assert isinstance(service.b, _ImplementationB)
+
+    async with container.context() as ctx:
+        service = await ctx.resolve(_NeedsMultipleImplementations)
+        assert isinstance(service.a, _ImplementationA)
+        assert isinstance(service.b, _ImplementationB)
