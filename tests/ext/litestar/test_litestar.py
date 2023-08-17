5c6eb9a4594d464cfb54c104765dbc3e61ec94a3
Doctor <thirvondukr@gmail.com>
2023-08-17 20:27:19 +0300

    Add basic litestar v2 integration

---
+++ tests/ext/litestar/test_litestar.py
@@ -0,0 +1,28 @@
+import uuid
+
+import httpx
+import pytest
+
+import aioinject
+
+pytestmark = [pytest.mark.anyio]
+
+
+async def test_function_route(
+    http_client: httpx.AsyncClient,
+    provided_value: int,
+) -> None:
+    response = await http_client.get("/function-route")
+    assert response.status_code == httpx.codes.OK.value
+    assert response.json() == {"value": provided_value}
+
+
+async def test_function_route_override(
+    http_client: httpx.AsyncClient,
+    container: aioinject.Container,
+) -> None:
+    expected = str(uuid.uuid4())
+    with container.override(aioinject.Object(expected, type_=int)):
+        response = await http_client.get("/function-route")
+    assert response.status_code == httpx.codes.OK.value
+    assert response.json() == {"value": expected}
