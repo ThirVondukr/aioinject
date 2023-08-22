15351734346181295875738a1d50d139b12bf4f9
Doctor <thirvondukr@gmail.com>
2023-08-22 18:44:17 +0300

    Add test for strawberry dataloaders

--- tests/ext/conftest.py
+++ tests/ext/conftest.py
@@ -8,8 +8,14 @@ def provided_value() -> int:
     return 42
 
 
+class NumberService:
+    async def get_number(self, number: int) -> int:
+        return number
+
+
 @pytest.fixture(scope="session")
 def container(provided_value: int) -> aioinject.Container:
     container = aioinject.Container()
     container.register(aioinject.Object(provided_value))
+    container.register(aioinject.Callable(NumberService))
     return container
--- tests/ext/strawberry/conftest.py
+++ tests/ext/strawberry/conftest.py
@@ -1,41 +1,19 @@
 from collections.abc import AsyncIterator
-from typing import Annotated, Any
+from typing import Any
 
 import httpx
 import pytest
-import strawberry
 from strawberry import Schema
 from strawberry.asgi import GraphQL
 
 import aioinject
-from aioinject import Inject
-from aioinject.ext.strawberry import ContainerExtension, inject
+from aioinject.ext.strawberry import ContainerExtension
+from tests.ext.strawberry.app import StrawberryApp, _Query
 
 
 @pytest.fixture
 def anyio_backend() -> str:
     return "asyncio"
-
-
-@strawberry.type
-class _Query:
-    @strawberry.field
-    @inject
-    async def hello_world(
-        self,
-        argument: str,
-        provided_value: Annotated[int, Inject],
-    ) -> str:
-        return f"{argument}-{provided_value}"
-
-    @strawberry.field
-    @inject
-    def hello_world_sync(
-        self,
-        argument: str,
-        provided_value: Annotated[int, Inject],
-    ) -> str:
-        return f"{argument}-{provided_value}"
 
 
 @pytest.fixture
@@ -44,7 +22,7 @@ async def app(container: aioinject.Container) -> GraphQL[Any, Any]:
         query=_Query,
         extensions=[ContainerExtension(container=container)],
     )
-    return GraphQL(schema=schema)
+    return StrawberryApp(schema=schema)
 
 
 @pytest.fixture
