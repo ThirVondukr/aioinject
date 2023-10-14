4d3f941e6aefe6e9fa677de1f6d2fa5afe824b87
Doctor <thirvondukr@gmail.com>
2023-10-15 02:27:04 +0300

    Import `Factory` provider in `__init__`

--- aioinject/__init__.py
+++ aioinject/__init__.py
@@ -2,19 +2,20 @@ from aioinject.containers import Container
 from aioinject.context import InjectionContext, SyncInjectionContext
 from aioinject.decorators import inject
 from aioinject.markers import Inject
-from aioinject.providers import Callable, Object, Provider, Singleton
+from aioinject.providers import Callable, Factory, Object, Provider, Singleton
 
 
 __all__ = [
-    "Container",
-    "InjectionContext",
-    "SyncInjectionContext",
-    "inject",
-    "Inject",
     "Callable",
+    "Container",
+    "Factory",
+    "Inject",
+    "InjectionContext",
+    "Object",
     "Provider",
     "Singleton",
-    "Object",
+    "SyncInjectionContext",
+    "inject",
 ]
 
 __version__ = "0.13.0"
